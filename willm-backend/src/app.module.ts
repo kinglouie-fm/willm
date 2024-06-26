358fd96a12f370f64c62374efc86509dd4430f0a
Ben Thillen <79511548+kinglouie-fm@users.noreply.github.com>
2024-06-26 23:25:48 +0200

    bug fix for authentication through mongodb

--- willm-backend/src/app.module.ts
+++ willm-backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { CorrectionModule } from './correction/correction.module';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING), CorrectionModule, UserModule],
+    ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING), CorrectionModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

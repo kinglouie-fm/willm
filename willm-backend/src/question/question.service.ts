ebb1ce5cd647b94c011c1e4e2f944db6e4ca326e
Ben Thillen <79511548+kinglouie-fm@users.noreply.github.com>
2024-07-18 11:11:21 +0200

    added comment to question service balance function

--- willm-backend/src/question/question.service.ts
+++ willm-backend/src/question/question.service.ts
@@ -70,7 +70,7 @@ export class QuestionService {
     return questionType;
   }
 
-  // question type's generation count is within a balanced range by comparing it to the average count 
+  // checks if question type's generation count is within a balanced range by comparing it to the average count 
   // of all question types plus a threshold.
   private async isBalanced(questionType: string): Promise<boolean> {
     const counts = await this.questionCountModel.find().exec();

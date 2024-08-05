5b7a72553de5d8cc8a5db957ec01d1b624381851
Ben Thillen <79511548+kinglouie-fm@users.noreply.github.com>
2024-08-05 23:01:10 +0200

    adjusted for prod

--- willm-flask-api/app.py
+++ willm-flask-api/app.py
@@ -728,4 +728,4 @@ def llm_decide_questions(new_questions, quiz_history):
     return []
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8031)

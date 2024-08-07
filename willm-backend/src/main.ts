4ded08db08c0567b4fa8cab5649784fd4b778ec2
Ben Thillen <79511548+kinglouie-fm@users.noreply.github.com>
2024-08-07 09:06:56 +0200

    adjusted port in main.ts

--- willm-backend/src/main.ts
+++ willm-backend/src/main.ts
@@ -9,10 +9,10 @@ async function bootstrap() {
   });
   app.use(cookieParser())
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://willm.corinth.informatik.rwth-aachen.de',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(3103);
+  await app.listen(3101);
 }
 bootstrap();

bf4ce6146354952d8f4c49d59b06b329886e5c35
Ben Thillen <79511548+kinglouie-fm@users.noreply.github.com>
2024-08-09 08:43:37 +0200

    adjusted unhighlighted mistakes location

--- willm-frontend/src/components/Evaluation.vue
+++ willm-frontend/src/components/Evaluation.vue
@@ -4,13 +4,18 @@ import Correction from './Correction.vue';
 
 const props = defineProps({
     scores: Object,
-    furtherCorrectionData: Object
+    furtherCorrectionData: Object,
+    unhighlightedMistakes: Array,
+    unhighlightedCorrections: Array,
+    unhighlightedExplanations: Array
 });
 </script>
 
 <template>
     <div>
         <Scores :scores="props.scores" class="mb-3" />
-        <Correction :furtherCorrectionData="props.furtherCorrectionData" />
+        <Correction :furtherCorrectionData="props.furtherCorrectionData" :unhighlightedMistakes="unhighlightedMistakes"
+            :unhighlightedCorrections="unhighlightedCorrections"
+            :unhighlightedExplanations="unhighlightedExplanations" />
     </div>
 </template>
